88b511f85614088a7216bd694ddef80fa8d56e5b
jk10274 <72466440+jk10274@users.noreply.github.com>
2021-04-17 11:28:15 +0200

    New Design

--- main.js
+++ main.js
@@ -13,3 +13,40 @@ function searching() {
         }
     }
 }
+
+$.ajax({
+    url: "/assets/web-app-list.json",
+    dataType: "json",
+}).done(function (result) {
+    console.log(result);
+    var selected = result.filter(function (item) {
+        return item.id == i;
+});
+
+var i;
+for (i = 0; i < result.length; i++) {
+    
+    const webappList = document.querySelector('#webappList');
+
+    let li = document.createElement('li');
+    let title = document.createElement('span');
+    let id = document.createElement('span');
+    let link = document.createElement('a');
+
+    var linkName = result[i].name;
+
+    li.className = "list-group-item";
+    title.textContent = result[i].title;
+    id.className = "text-secondary badge";
+    id.textContent = "AP" + result[i].id;
+    link.href = "https://apuem.com/" + linkName;
+
+    li.appendChild(title);
+    li.appendChild(id);
+
+    link.appendChild(li);
+
+    webappList.appendChild(link);
+}
+
+});
